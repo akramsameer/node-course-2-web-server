261dad07bf212f44af33d955fa010c24ef54763f
Akram <akram.sameer99@gmail.com>
2018-08-10 18:31:54 +0200

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req , res , next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
-
+ 
     console.log(log);
    
     fs.appendFile('server.log' , log + '\n', (err) =>{
